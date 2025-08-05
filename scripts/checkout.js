a06bed42015e2c61f4289c54c0cdd0a14b769600
getnet <getnetyideg54@gmail.com>
2025-08-04 23:20:58 -0100

    delete products when clicking delete

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -11,7 +11,7 @@ cart.forEach((cartItem)=>{
         }
     });
     cartSummaryHTML += `
-        <div class="cart-item-container">
+        <div class="cart-item-container js-cart-item-container-${matchingCart.id}">
             <div class="delivery-date">
               Delivery date: Wednesday, June 15
             </div>
@@ -92,6 +92,7 @@ document.querySelectorAll('.js-delete-link').forEach((deleteElement)=>{
     deleteElement.addEventListener('click', ()=>{
         let productId = deleteElement.dataset.productId;
         removeFromCart(productId);
-        console.log(cart);
+        const container = document.querySelector(`.js-cart-item-container-${productId}`);
+        container.remove();
     })
 })
